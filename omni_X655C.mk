86a6a861fe121cfb86f2c0262e2c0bd7dfe58ec6
kies83 <57283487+kies83@users.noreply.github.com>
2022-08-03 19:06:33 +0500

    Update omni_X655C.mk

--- omni_X655C.mk
+++ omni_X655C.mk
@@ -19,10 +19,11 @@
 DEVICE_PATH := device/infinix/X655C
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := X655C
